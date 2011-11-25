32f72e5f5242c5cc7c049243f9c478af920c4bc1
Dinoja Padmanabhan <dinoj18@gmail.com>
2011-11-25 13:48:07 -0500

    Distance vector Routing Algorithm implementation

--- distance_vector.h
+++ distance_vector.h
@@ -1,16 +1,23 @@
 #ifndef __DISTANCE_VECTOR_H__
 #define __DISTANCE_VECTOR_H__
 
+#include <vector>
+
+using namespace std;
+
 class distance_vector_t
 {
   public:
   // member variables
+  vector < vector < vector<float> > > D;
+  vector < vector<bool> > B;
 
   // constructor and destructor
-  distance_vector_t();
+  distance_vector_t(router_t&);
   ~distance_vector_t();
 
   // driver functions
+  int send_dv_neighbors(router_t&, unsigned int);
   int compute_distance_vector(router_t&);
 
   static void distance_vector_usage();
